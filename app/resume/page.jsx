798929969aa0d1a6c1e0beab6c22d7c5195605a4
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2024-12-22 05:23:54 +0100

    Update page.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -274,7 +274,7 @@ export default function Resume() {
                     {education.items.map((item, index) => (
                       <li
                         key={index}
-                        className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        className="bg-[#232329] h-auto py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                       >
                         <span className="text-accent">{item.duration}</span>
                         <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
