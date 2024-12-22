02a899b41313eff80d7fc3bad9eb494d5aec7492
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2024-12-22 03:20:01 +0100

    Update Loading.jsx

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -24,7 +24,7 @@ export default function Loading() {
       }}
       className="flex  justify-center items-center opacity-70 fixed top-0 left-0 right-0 h-screen z-10 w-screen"
     >
-      <span className="text-xl xl:text-3xl">Loading...</span>
+      <span className="text-xl xl:text-3xl">Submitting...</span>
     </motion.div>
   );
 }
