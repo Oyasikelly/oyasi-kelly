f811bcc91084677a23b5e559b84a47fabf156acd
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2024-12-22 03:15:43 +0100

    Updated Contact page and added a loading component

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>
+      <body className={jetbrainsMono.variable} style={{ position: "relative" }}>
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
