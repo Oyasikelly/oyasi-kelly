e1f6847d0af6275b25bd9c71b6fd893aba415f50
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2025-09-10 00:26:36 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -51,7 +51,7 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="w-full justify-center items-center py-4">
+			<div className="w-full flex justify-center items-center py-4">
 				<Button
 								variant="outline"
 								size="md"
